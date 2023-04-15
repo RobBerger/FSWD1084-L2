5de54763c38f2dfa92412159da7348e55b364bc1
Robert Berger <robertberger.dev@gmail.com>
2023-04-15 19:56:52 -0400

    set up server

--- app.ts
+++ app.ts
@@ -1,3 +1,11 @@
 import * as http from 'http';
 
 const port = 3000;
+
+const server = http.createServer((req: http.IncomingMessage, res: http.ServerResponse) => {
+    res.end('Hello World');
+});
+
+server.listen(port, () => {
+    console.log(`Server listeneing on port ${port}`)
+})
