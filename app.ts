a971fef173d9a34461fe9cc1d11a717ccad8a614
Robert Berger <robertberger.dev@gmail.com>
2023-04-17 16:24:30 -0400

    updated app.ts

--- app.ts
+++ app.ts
@@ -1,47 +1,10 @@
 import * as http from 'http';
+import reqHandler from './routes';
 
 const port = 3000;
 
-const server = http.createServer((req: http.IncomingMessage, res: http.ServerResponse) => {
-    const url = req.url;
-    const method = req.method;
-
-    if (url === '/message' && method === 'GET') {
-        res.setHeader('Content-Type', "text/html");
-        res.write("<html>");
-        res.write("<head><title>Node Server</title></head>");
-        res.write("<body>");
-        res.write("<h2>Message Form</h2>");
-        // form tag updated with the method and action attributes
-        res.write(`<form method="POST" action="/message"><input type="text" name="message"><button>Submit</button></form>`)
-        res.write("</body>");
-        res.write("</html>");
-        return res.end();
-    }
-    else if (url === '/message' && method == 'POST') {
-        const body: Buffer[] = [];
-
-        req.on('data', function (datachunk) {
-            console.log(datachunk);
-            body.push(datachunk);
-        });
-
-        req.on('end', function () {
-            const parsedBody = Buffer.concat(body).toString();
-            console.log(parsedBody);
-            const value = parsedBody.split("=")[1];
-            res.write("<html>");
-            res.write("<head><title>Node Server</title></head>");
-            res.write("<body>");
-            res.write("<div>Received Message Form</div>")
-            res.write(`<div>Message Content: ${value}</div>`); 
-            res.write("</body>");
-            res.write("</html>");
-            return res.end();
-        });
-    }
-});
+const server = http.createServer(reqHandler);
 
 server.listen(port, () => {
-    console.log(`Server listeneing on port ${port}`)
-})
+    console.log(`Server listening on port ${port}`)
+});
