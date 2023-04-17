1303a12731fe3dca705377f6c6a738708e7d8b3c
Robert Berger <robertberger.dev@gmail.com>
2023-04-17 13:06:21 -0400

    changed console.logs to give more precise data

--- app.ts
+++ app.ts
@@ -3,9 +3,9 @@ import * as http from 'http';
 const port = 3000;
 
 const server = http.createServer((req: http.IncomingMessage, res: http.ServerResponse) => {
-    console.log("INCOMING MESSAGE REQUEST OBJECT");
-    console.log("-------------------------------");
-    console.log(req);
+    console.log(req.url);
+    console.log(req.method);
+    console.log(req.headers);
     
     res.end('Hello World');
 });
