ded7f6e522c78a9d9f1255141ab66ed2b703a49b
Robert Berger <robertberger.dev@gmail.com>
2023-04-17 13:46:42 -0400

    added res.write examples

--- app.ts
+++ app.ts
@@ -3,11 +3,12 @@ import * as http from 'http';
 const port = 3000;
 
 const server = http.createServer((req: http.IncomingMessage, res: http.ServerResponse) => {
-    console.log(req.url);
-    console.log(req.method);
-    console.log(req.headers);
+    res.write("<html>");
+    res.write("<head><title>Node Server</title></head>");
+    res.write("<body><h2>Hello World</h2></body>");
+    res.write("</html>");
     
-    res.end('Hello World');
+    res.end();
 });
 
 server.listen(port, () => {
