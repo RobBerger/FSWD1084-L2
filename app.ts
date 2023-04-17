3bf83e604230547b6c33786600bc407799d44c83
Robert Berger <robertberger.dev@gmail.com>
2023-04-17 15:45:51 -0400

    added if statment message example

--- app.ts
+++ app.ts
@@ -3,31 +3,26 @@ import * as http from 'http';
 const port = 3000;
 
 const server = http.createServer((req: http.IncomingMessage, res: http.ServerResponse) => {
-    // setting the response content type to text/html
-    res.setHeader('Content-Type', 'text/html');
-
-    // extracting the URL from the request object
     const url = req.url;
+    const method = req.method;
 
-    if (url === '/') {
-        // writing HTML content to be sent as response in multiple lines
-        res.write("<html>");
-        res.write("<head><title>Node Server</title></head>");
-        res.write("<body><h2>Hello World</h2></body>");
-        res.write("</html>");
-    }
-    else if (url === '/info') {
-        // writing HTML content to be sent as response in multiple lines
+    if (url === '/message' && method === 'GET') {
         res.write("<html>");
         res.write("<head><title>Node Server</title></head>");
         res.write("<body>");
-        res.write("<h2>Server Information</h2>");
-        res.write("<div>Server Type: Node.js</div><div>Language: TypeScript</div><div>Port: 3000</div>")
-        res.write("</body>")
+        res.write("<h2>Message Form</h2>");
+        // Using back ticks so we can include quotation marks
+        res.write(`<form><input type="text" name="message"><button type="submit">Submit</button></form>`)
+        res.write("</body>");
         res.write("</html>");
-    }
+        
+        return res.end()
+    } 
 
-    // ending the response
+    res.write("<html>");
+    res.write("<head><title>Node Server</title></head>");
+    res.write("<body><h2>Hello World</h2></body>");
+    res.write("</html>");
     res.end();
 });
 
