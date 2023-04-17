4249d4cb978309dab41b683bedf3363da8a6806e
Robert Berger <robertberger.dev@gmail.com>
2023-04-17 16:30:29 -0400

    installed nodemon

--- build/app.js
+++ build/app.js
@@ -22,45 +22,14 @@ var __importStar = (this && this.__importStar) || function (mod) {
     __setModuleDefault(result, mod);
     return result;
 };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const http = __importStar(require("http"));
+const routes_1 = __importDefault(require("./routes"));
 const port = 3000;
-const server = http.createServer((req, res) => {
-    const url = req.url;
-    const method = req.method;
-    if (url === '/message' && method === 'GET') {
-        res.setHeader('Content-Type', "text/html");
-        res.write("<html>");
-        res.write("<head><title>Node Server</title></head>");
-        res.write("<body>");
-        res.write("<h2>Message Form</h2>");
-        // form tag updated with the method and action attributes
-        res.write(`<form method="POST" action="/message"><input type="text" name="message"><button>Submit</button></form>`);
-        res.write("</body>");
-        res.write("</html>");
-        return res.end();
-    }
-    else if (url === '/message' && method == 'POST') {
-        const body = [];
-        req.on('data', function (datachunk) {
-            console.log(datachunk);
-            body.push(datachunk);
-        });
-        req.on('end', function () {
-            const parsedBody = Buffer.concat(body).toString();
-            console.log(parsedBody);
-            const value = parsedBody.split("=")[1];
-            res.write("<html>");
-            res.write("<head><title>Node Server</title></head>");
-            res.write("<body>");
-            res.write("<div>Received Message Form</div>");
-            res.write(`<div>Message Content: ${value}</div>`);
-            res.write("</body>");
-            res.write("</html>");
-            return res.end();
-        });
-    }
-});
+const server = http.createServer(routes_1.default);
 server.listen(port, () => {
-    console.log(`Server listeneing on port ${port}`);
+    console.log(`Server listening on port ${port}`);
 });
